b7970eed161dbf1920f550abb53332eabb5455d7
Matiassgg <matugrillo@hotmail.com>
2021-04-17 22:45:21 -0300

    Borro Clase1 y creo la Clase Prenda

---
+++ src/main/java/Prenda.java
@@ -0,0 +1,52 @@
+import Exepciones.PrendaInvalidaException;
+
+public class Prenda {
+  TipoPrenda tipo;
+  MaterialPrenda material;
+  ColorPrimario colorPrimario;
+  ColorSecundario colorSecundario;
+
+  // Este constructor es el que utiliza el usuario para crear sus prendas especificando que desea cargar
+  public Prenda(
+      CategoriaPrenda categoria,
+      TipoPrenda tipo,
+      MaterialPrenda material,
+      ColorPrimario colorPrimario,
+      ColorSecundario colorSecundario
+  ) {
+    this.chequearDatosNulos(categoria, tipo, material, colorPrimario);
+    this.chequearTipo(categoria, tipo);
+
+    // Si hubiera que chequear mas cosas (colorPrimario valido, material valido)
+    // agregaría métodos dentro del constructor del estilo 'chequearColor(color)' ...
+
+    this.tipo = tipo;
+    this.material = material;
+    this.colorPrimario = colorPrimario;
+    this.colorSecundario = colorSecundario;
+
+    // La prenda no lanzo ninguna excepción al momento de crearla
+    System.out.println("Se creo la prenda correctamente");
+  }
+
+  private void chequearDatosNulos(
+      CategoriaPrenda categoria,
+      TipoPrenda  tipo,
+      MaterialPrenda material,
+      ColorPrimario colorPrimario
+  ) {
+    if(categoria == null || tipo == null || material == null || colorPrimario == null) {
+      // Falto ingresar algún dato, lanzo una excepción
+      throw new PrendaInvalidaException("el ingreso de datos fue incompleto");
+    }
+  }
+
+  private void chequearTipo(CategoriaPrenda categoria, TipoPrenda tipo) {
+    if(!tipo.esValidaLa(categoria)) {
+      // En este caso, el tipo no se condice con su categoria
+      throw new PrendaInvalidaException("el tipo no se condice con su categoria");
+    }
+  }
+
+}
+
